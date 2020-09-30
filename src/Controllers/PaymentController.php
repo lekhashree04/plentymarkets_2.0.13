758126fb547fdaf765a0dc7b9d86544424ead50d
lekhashree04 <71366428+lekhashree04@users.noreply.github.com>
2020-09-30 12:27:08 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -25,6 +25,7 @@ use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 use Novalnet\Services\PaymentService;
 use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\ConfigRepository;
+use Plenty\Plugin\Log\Loggable;
 
 
 /**
@@ -35,6 +36,7 @@ use Plenty\Plugin\ConfigRepository;
 class PaymentController extends Controller
 {
     
+     use Loggable;
     /**
      * @var Request
      */
